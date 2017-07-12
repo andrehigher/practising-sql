9a0b0793f163410f8fe60a097eec1aa4ab5da627
André Gonzaga <andre.gonzaga@bloomingdales.com>
2017-07-11 22:50:20 -0300

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: "./client/main.js",
+    output: {
+        path: '/Users/andre/workspace/practising-sql/public/dist/',
+        filename: "bundle.js",
+        publicPath: '/'
+    },
+    devServer: {
+        inline: false,
+        contentBase: "./dist",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude:/(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+
+};
